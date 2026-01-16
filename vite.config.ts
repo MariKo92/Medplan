f21b77f56f51b9b2512296474d4a21e2bce2c0b4
Marianne Svendsen Kofoed <marianne.kofoed@bouvet.no>
2026-01-16 15:05:29 +0100

    Update repository name to medplan

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nedtrappingsplan-for-medikamenter/',
+  base: '/medplan/',
   server: {
     port: 3000,
   },
